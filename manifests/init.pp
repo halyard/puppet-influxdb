b163f36dcaeb684e60d01c59995ce6928a335f4a
Les Aker <me@lesaker.org>
2022-05-10 22:12:06 -0400

    more magic

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,31 @@
 # @summary Configure InfluxDB instance
 #
 class influxdb (
+  String $hostname,
+  String $datadir,
+  String $tls_account,
+  Optional[String] $challengealias = undef,
 ) {
-  include docker
-  include acme
+  file { ["${datadir}", "${datadir}/data", "${datadir}/certs"]:
+    ensure => directory,
+  }
+
+  -> file { "${datadir}/config.yml":
+    ensure  => file,
+    content => template('influxdb/config.yml.erb'),
+  }
+
+  -> acme::certificate { $hostname:
+    reloadcmd      => '/usr/bin/systemctl restart container@influxdb',
+    keypath        => "${datadir}/certs/key",
+    fullchainpath  => "${datadir}/certs/cert",
+    account        => $tls_account,
+    challengealias => $tls_challengealias,
+  }
+
+  -> docker::container { 'influxdb':
+    image => 'influxdb:latest',
+    args  => "-p 127.0.0.1:8086:8086 -v ${datadir}/data:/var/lib/influxdb2 -v ${datadir}/config.yml:/etc/influxdb2/config.yml",
+    cmd   => '',
+  }
 }
