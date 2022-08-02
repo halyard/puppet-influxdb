3fd99dfd5a1b4473f5a68bf176ce1b9170f4f5a7
Les Aker <me@lesaker.org>
2022-08-01 22:10:50 -0400

    now with smarter firewalls

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,11 +4,13 @@
 # @param datadir sets where the data is persisted
 # @param tls_account sets the TLS account config
 # @param tls_challengealias sets the alias for TLS cert
+# @param container_ip sets the address of the Docker container
 class influxdb (
   String $hostname,
   String $datadir,
   String $tls_account,
   Optional[String] $tls_challengealias = undef,
+  String $container_ip = '172.17.0.2',
 ) {
   file { ["${datadir}/data", "${datadir}/certs"]:
     ensure => directory,
@@ -27,10 +29,19 @@ class influxdb (
     challengealias => $tls_challengealias,
   }
 
+  -> firewall { '100 dnat for influxdb':
+    chain  => 'DOCKER_EXPOSE',
+    jump   => 'DNAT',
+    proto  => 'tcp',
+    dport  => 443,
+    todest => "${container_ip}:8086",
+    table  => 'nat',
+  }
+
   -> docker::container { 'influxdb':
     image => 'influxdb:latest',
     args  => [
-      '-p 443:8086',
+      "--ip ${container_ip}",
       "-v ${datadir}/data:/var/lib/influxdb2",
       "-v ${datadir}/config.yml:/etc/influxdb2/config.yml",
       "-v ${datadir}/certs:/mnt/certs",
